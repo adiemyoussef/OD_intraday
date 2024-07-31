14d13106002fad5a258481e6b84c4490832df346
youssefadiem <youssef.adiem.1@ens.etsmtl.ca>
2024-07-31 00:43:30 -0400

    changed DB_PORT

--- config/config.py
+++ config/config.py
@@ -8,7 +8,7 @@ nyse = mcal.get_calendar('NYSE')
 #---------------- DIGITAL OCEAN ---------------------------#
 # # - MySQL
 DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+DB_PORT = int(os.getenv('DB_PORT'))
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
