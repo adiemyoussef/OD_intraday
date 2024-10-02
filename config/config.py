eaeabfd4199bd2eedfa5faac929abf73047580f4
youssefadiem <youssef.adiem.1@ens.etsmtl.ca>
2024-10-02 14:35:27 -0400

    changed override_entries setting

--- config/config.py
+++ config/config.py
@@ -140,13 +140,13 @@ WEBHOOK_URLS = {
     'zero_dte': 'https://discord.com/api/webhooks/1273463250230444143/74Z8Xo4Wes7jwzdonzcLZ_tCm8hdFDYlvPfdTcftKHjkI_K8GNA1ZayQmv_ZoEuie_8_',
     'one_dte': 'https://discord.com/api/webhooks/1275269470151245938/qNZXtA_ySwcJJJf6bS_myYqU-uDd71zHV--XJBR7xb6uVhs7ccjKE59_c8y9AMZ86OC_',
     'gex': 'https://discord.com/api/webhooks/1277599354932428842/c2Ix3cPdLzI0fzxDdoGRye8nyKPLZj0dqmIxOiRQP2DYFx7YbgphUe8rAsWqkZUKiD0f',
-    'depthview': 'https://discord.com/api/webhooks/1290327506741629121/eoNGb8jZhlgmnDYyuKYBk90wm6it8atN88B1kcCJxZbyNCgZqQ5bJC2_pvZZDThIMi6I',
+    'depthview': 'TOTO',
     'options_charts': 'TOTO',
     'dev': 'https://discord.com/api/webhooks/1290327506741629121/eoNGb8jZhlgmnDYyuKYBk90wm6it8atN88B1kcCJxZbyNCgZqQ5bJC2_pvZZDThIMi6I',
     'test-0dte': 'https://discord.com/api/webhooks/1274040299735486464/Tp8OSd-aX6ry1y3sxV-hmSy0J3UDhQeyXQbeLD1T9XF5zL4N5kJBBiQFFgKXNF9315xJ',
     'test-1dte': 'https://discord.com/api/webhooks/1290326924232753202/AJYU_-tIX9lsLuRTiz21JBdhnT2HL6FNqTTAlETfRymKQvAIyRtPORHe9IUc2SQri9bW',
     'test-gex-0dte': 'https://discord.com/api/webhooks/1290327206006100018/aaFlt1_zGCYiAinB1PysmA96BCK6Yrn839g-omhyHjw2CaCR8btmesTRy8ij5cdqoW-E',
-
+    'test-depthview': 'https://discord.com/api/webhooks/1291105984235962389/foibAP6OtQ37rrm6HzIMCFNgP399BJtQ53ZYD816pH8lUb6i6w1oS7b-61a0X4aclXWM',
 }
 
 class WebhookUrl(Enum):
